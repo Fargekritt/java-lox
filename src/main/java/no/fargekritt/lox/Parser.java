192ffd66c7ffd4ef81444a46cd7f3c32977e0019
Fargekritt <Lullinj98@gmail.com>
2024-05-29 18:11:40 +0200

    Now parser ,

--- src/main/java/no/fargekritt/lox/Parser.java
+++ src/main/java/no/fargekritt/lox/Parser.java
@@ -30,7 +30,9 @@ public class Parser {
     private Expr comma(){
         Expr expr = equality();
         while (match(COMMA)){
-
+            Token operator = previous();
+            Expr right = equality();
+            expr = new Expr.Binary(expr, operator, right);
         }
         return expr;
     }
